dfb1612831defe2a4216843231b8535e8a6ea34e
Lisa M. Venezia <leftylmv78@gmail.com>
2018-11-19 22:25:13 -0500

    feature: add hamburger menu with toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  /* function to toggle the sidebar */
+  toggleSidenav() {
+    document.body.classList.toggle("sidenav-active");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/* Hamburger menu */}
+        <div
+          className="hamburger"
+          id="hamburger"
+          onClick={event => this.toggleSidenav(event)}
+        >
+          <div> </div>
+          <div> </div>
+          <div> </div>
+        </div>
+
+        {/* Sidebar Navigation */}
+        <nav>
+          <div className="links">
+            <a className="active" href="/">
+              Home
+            </a>
+            <a href="/">Location 1</a>
+          </div>
+        </nav>
+
+        {/* Main Page */}
+        <div className="main">
+          <section id="map">
+            <h1>This is where the map will go!</h1>
+          </section>
+        </div>
       </div>
     );
   }
